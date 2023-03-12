b4d31e54852b6283d83920f288e65ac7680eb7f8
Kirill Aleksandrovich <KArtamonov1@yandex.ru>
2023-03-12 16:30:18 +0300

    Delivery homework_m3 1.3

--- homework_m3/m3_1.py
+++ homework_m3/m3_1.py
@@ -1,25 +1,38 @@
 # # m3_1_1 level 1
-# def precent(x, p, y):
-#     f = ((x / 100) * p) * y
-#     print(f'Вклад за {y} лет/год, будет не менее {f:.0f} рублей.')
+def precent(x, p, y):
+    f = ((x / 100) * p) * y
+    print(f'Вклад за {y} лет/год, будет не менее {f:.0f} рублей.')
 
 
-# def calculation():
-#     precent(
-#         x=float(input('Contribution: ')),
-#         p=float(input('Precent: ')),
-#         y=int(input('Years: '))
-#     )
+def calculation():
+    precent(
+        x=float(input('Contribution: ')),
+        p=float(input('Precent: ')),
+        y=int(input('Years: '))
+    )
 
 
-# calculation()
+calculation()
 
 
 # m3_1_2 level 2
-
 i = int(input('Start: '))
 f = int(input('Stop: '))
 
 while i <= f:
     print('for - как частный случай while')
     i += 1
+
+
+# m3_1_3 level 3
+def sum_of_digits(num):
+    sum = 0
+    while num > 0:
+        sum += num % 10
+        num //= 10
+    return sum
+
+
+print(sum_of_digits(int(input('Enter num please: '))))
+# Another example
+print(sum(map(int,input('Enter num please: '))))
