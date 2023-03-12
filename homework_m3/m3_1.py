fc9838c55e6d6a4784363690ec86431eeb9a1682
Kirill Aleksandrovich <KArtamonov1@yandex.ru>
2023-03-12 16:08:41 +0300

    Delivered homework_m3 1.2

--- homework_m3/m3_1.py
+++ homework_m3/m3_1.py
@@ -1,15 +1,25 @@
-# m3_1_1
-def precent(x, p, y):
-    f = ((x / 100) * p) * y
-    print(f'Вклад за {y} лет/год, будет не менее {f:.0f} рублей.')
+# # m3_1_1 level 1
+# def precent(x, p, y):
+#     f = ((x / 100) * p) * y
+#     print(f'Вклад за {y} лет/год, будет не менее {f:.0f} рублей.')
 
 
-def calculation():
-    precent(
-        x=float(input('Contribution: ')),
-        p=float(input('Precent: ')),
-        y=int(input('Years: '))
-    )
+# def calculation():
+#     precent(
+#         x=float(input('Contribution: ')),
+#         p=float(input('Precent: ')),
+#         y=int(input('Years: '))
+#     )
 
 
-calculation()
+# calculation()
+
+
+# m3_1_2 level 2
+
+i = int(input('Start: '))
+f = int(input('Stop: '))
+
+while i <= f:
+    print('for - как частный случай while')
+    i += 1
